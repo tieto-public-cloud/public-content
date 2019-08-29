6f8014b65cfc4ab9170d4ac8a57e1d864d8bd1f2
Ondřej Václavů <46448507+vaclaond@users.noreply.github.com>
2019-08-29 11:09:56 +0200

    Create Import-TrustedRootCertificate.ps1

---
+++ scripts/Import-TrustedRootCertificate.ps1
@@ -0,0 +1,22 @@
+Param(
+    [Parameter(Mandatory = $false)]
+    [string]
+    $BaseUri = "https://sslinspectionstorage2908.blob.core.windows.net/certs",
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $CertName = "CACert.crt" # Root public certificate file name 
+)
+
+# Script variables (do not modify)
+$certUri = "$BaseUri/$CertName"
+$tempFile = "$env:temp\$CertName"
+
+# Download public certificate from storage account and save in temp location
+Invoke-WebRequest -Uri $certUri -OutFile $tempFile
+
+# Import certificate to the trusted root certificates
+Import-Certificate -FilePath $tempFile -CertStoreLocation Cert:\LocalMachine\Root
+
+# Remove temp file
+Remove-Item $tempFile
